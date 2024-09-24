37298b6e66568a4356e8e018169359042d6ac7e8
Georg <georgarshakyan@yandex.ru>
2024-09-25 01:41:03 +0300

    Start js, structure css components

---
+++ public/script.js
@@ -0,0 +1,12 @@
+const root = document.getElementById('window');
+
+const loginElement = document.createElement('a');
+loginElement.href = '/login';
+loginElement.textContent = 'Вход';
+loginElement.className()
+
+const signupElement = document.createElement('a');
+signupElement.href = '/signup';
+signupElement.textContent = 'Регистрация';
+
+root.appendChild(loginElement);
